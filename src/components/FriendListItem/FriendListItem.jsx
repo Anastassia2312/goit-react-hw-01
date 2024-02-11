608d4ac13a2ede79a39c0343283522a4e362d0b3
Anastassia2312 <23122000na@gmail.com>
2024-02-11 15:12:20 +0200

    statusStyles

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,12 @@
 import css from "./FriendListItem.module.css";
 import clsx from "clsx";
 export const FriendListItem = ({ avatar, name, isOnline }) => {
+  const statusStyles = clsx(isOnline ? css.onlineStyles : css.offlineStyles);
   return (
     <div className={css.listContainer}>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p>{isOnline ? "Online" : "Offline"}</p>
+      <p className={statusStyles}>{isOnline ? "Online" : "Offline"}</p>
     </div>
   );
 };
