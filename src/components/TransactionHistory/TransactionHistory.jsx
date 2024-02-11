65fe7fe43b898538ff642df60453564b850bc05c
Anastassia2312 <23122000na@gmail.com>
2024-02-11 16:12:31 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -13,9 +13,9 @@ export const TransactionHistory = ({ items }) => {
       <tbody>
         {items.map((item) => (
           <tr className={css.trTable} key={item.id}>
-            <td>{item.type}</td>
-            <td>{item.amount}</td>
-            <td>{item.currency}</td>
+            <td className={css.tdTable}>{item.type}</td>
+            <td className={css.tdTable}>{item.amount}</td>
+            <td className={css.tdTable}>{item.currency}</td>
           </tr>
         ))}
       </tbody>
