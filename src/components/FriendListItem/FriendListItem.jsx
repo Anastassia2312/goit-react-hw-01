2ce1cd2d31b68d6de9127acfbb0b1a337a987ddc
Anastassia2312 <23122000na@gmail.com>
2024-02-11 14:29:54 +0200

    Summary

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,6 +1,8 @@
+import css from "./FriendListItem.module.css";
+import clsx from "clsx";
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <div>
+    <div className={css.listContainer}>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
       <p>{isOnline ? "Online" : "Offline"}</p>
